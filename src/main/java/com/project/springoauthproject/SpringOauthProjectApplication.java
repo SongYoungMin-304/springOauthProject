1865da4cb3f7f9f211e50fd0dcf41db89985200f
82108 <dudals789>
2022-02-23 10:35:55 +0900

    Initial commit

---
+++ src/main/java/com/project/springoauthproject/SpringOauthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.springoauthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauthProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/springoauthproject/SpringOauthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springoauthproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
